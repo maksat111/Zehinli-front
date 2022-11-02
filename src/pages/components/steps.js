979faccc527cbefeae05fb4815c2d8c8e9ad1ddd
Maksat Akmyradow <i.am.maxat@gmail.com>
2022-11-02 16:49:47 +0500

    changed to whileInwiev in framermotion

--- src/pages/components/steps.js
+++ src/pages/components/steps.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
-const StepCard = ({ icon, stepHeader, stepDescription, rocket }) => {
+const StepCard = ({ icon, stepHeader, stepDescription, rocket, duration }) => {
     return (
-        <div className='bg-white w-[20vw] h-[30vh] shadow-new2 rounded-[20px] px-6'>
+        <motion.div initial={{ y: 200 }} whileInView={{ y: 0 }} transition={{ duration }} className='bg-white w-[20vw] h-[30vh] shadow-new2 rounded-[20px] px-6'>
             <img className={`relative h-[19vh] mt-[-7vh] ${rocket ? 'left-9vw' : 'left-7vw'}`} src={icon} alt='Rocket' />
             <div className='flex flex-wrap justify-items-end pt-[20px]'>
                 <h2 className='font-lato text-new-color font-[600] pb-[10px]'>{stepHeader}</h2>
                 <p>{stepDescription}</p>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/pages/sections/steps.js
+++ src/pages/sections/steps.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import StepCard from '../components/steps';
 import raketa from '../../img/raketa.png';
 import question from '../../img/question.png';
@@ -10,15 +11,15 @@ const Steps = () => {
     return (
         <div id='about' className='h-100vh'>
             <div className='flex flex-col text-center pt-[120px] pb-[140px]'>
-                <p className='font-normal font-poppins text-customGrey text-new-color py-[10px]'>Bäsleşik nähili geçýär?</p>
-                <h2 className='font-extrabold font-lato text-5xl text-new-color'>Oýun 3 basgançakdan durýar</h2>
+                <motion.p initial={{ x: -500 }} whileInView={{ x: 0 }} transition={{ duration: 0.7 }} className='font-normal font-poppins text-customGrey text-new-color py-[10px]'>Bäsleşik nähili geçýär?</motion.p>
+                <motion.h2 initial={{ x: -700 }} whileInView={{ x: 0 }} transition={{ duration: 1 }} className='font-extrabold font-lato text-5xl text-new-color'>Oýun 3 basgançakdan durýar</motion.h2>
             </div>
             <div className='flex flex-row justify-around'>
-                <StepCard icon={raketa} rocket={true} stepHeader={'Bäsleşigi telefonyňa ýükle'} stepDescription={'Bir bäsleşige gatnaşmak üçin 2 tmt töleg geçmeli'} />
+                <StepCard icon={raketa} rocket={true} duration={0.4} stepHeader={'Bäsleşigi telefonyňa ýükle'} stepDescription={'Bir bäsleşige gatnaşmak üçin 2 tmt töleg geçmeli'} />
                 {/* <img className='h-5 w-5vw' src={icon1} alt='icon1' /> */}
-                <StepCard icon={question} stepHeader={'Soraglara jogap ber'} stepDescription={'Soraglara dogry we tiz jogap bermäge çalyşyň'} />
+                <StepCard icon={question} duration={0.6} stepHeader={'Soraglara jogap ber'} stepDescription={'Soraglara dogry we tiz jogap bermäge çalyşyň'} />
                 {/* <img className='h-5 w-5vw' src={icon2} alt='icon2' /> */}
-                <StepCard icon={kubok} stepHeader={'Baýrakly orunlara mynasyp bol'} stepDescription={'Her gün gymmatbahaly baýrak gazanmak — size bagly'} />
+                <StepCard icon={kubok} duration={0.8} stepHeader={'Baýrakly orunlara mynasyp bol'} stepDescription={'Her gün gymmatbahaly baýrak gazanmak — size bagly'} />
             </div>
         </div>
     );
