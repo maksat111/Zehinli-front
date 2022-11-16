236b74909d5a74756a1f651702ab4e2ffb53015e
maksat111 <maksatakmyradow111@gmail.com>
2022-11-16 17:02:25 +0500

    download and footer

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -14,7 +14,7 @@ const HomePage = () => {
             <Steps />
             <Prizes />
             <Download />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
--- src/pages/sections/download.js
+++ src/pages/sections/download.js
@@ -8,18 +8,18 @@ import appStore from '../../img/appStore.png';
 
 const Download = () => {
     return (
-        <div id='download' className='flex items-center h-80vh'>
-            <motion.div initial={{ y: 200 }} whileInView={{ y: 0 }} transition={{ duration: 0.5 }} className='grid grid-cols-2 bg-gradient-to-r from-[#4F9AEB] to-[#007BFF] rounded-[20px] h-[50vh]'>
-                <motion.div initial={{ y: 200 }} whileInView={{ y: 0 }} transition={{ duration: 0.7 }} className='flex flex-col items-center justify-center h-50vh w-[47vw]'>
-                    <h2 className='text-[#F3F8FF] text-[45px] font-[800] font-lato'>Taýýarmy?<br />Zehinli Bäsleşigi <br /> dessine ýükle</h2>
+        <div id='download' className='flex items-center w-90vw lg:h-80vh'>
+            <motion.div initial={{ y: 200 }} whileInView={{ y: 0 }} transition={{ duration: 0.5 }} className='bg-gradient-to-r from-[#4F9AEB] to-[#007BFF] rounded-[20px] flex flex-row-reverse justify-center w-[90vw] lg:grid lg:grid-cols-2 lg:h-[50vh]'>
+                <motion.div initial={{ y: 200 }} whileInView={{ y: 0 }} transition={{ duration: 0.7 }} className='z-1 flex flex-col items-center justify-center h-50vh w-90vw lg:w-[47vw]'>
+                    <h2 className='text-[#F3F8FF] text-[45px] font-[800] font-lato'>Taýýarmy? Zehinli Bäsleşigi dessine ýükle</h2>
                     <div className='flex flex-wrap gap-8 justify-start p-7'>
                         <DownloadButton icon={googlePlay} alt={'Google Play'} />
                         <DownloadButton icon={appStore} alt={'App Store'} />
                     </div>
                 </motion.div>
-                <div className='flex gap-[-100px] h-50vh w-[47vw]'>
-                    <motion.img initial={{ rotate: 45, scale: 1.6, y: 200, x: -200 }} whileInView={{ rotate: 0, y: 0, x: 0, scale: 1 }} transition={{ duration: 0.8 }} className='relative h-[81vh]' src={phone} alt='Phone' />
-                    <motion.img initial={{ rotate: -45, scale: 1.6, y: 200, x: 200 }} whileInView={{ rotate: 0, y: 0, x: 0, scale: 1 }} transition={{ duration: 0.8 }} className='relative z-2 h-[81vh]' src={leaderboard} alt='Phone' />
+                <div className='z-2 absolute flex lg:gap-[-100px] justify-center h-50vh w-80vw lg:w-[47vw]'>
+                    <motion.img initial={{ rotate: 45, scale: 1.6, y: 200, x: -200 }} whileInView={{ rotate: 0, y: 0, x: 0, scale: 1 }} transition={{ duration: 0.8 }} className='relative min-w-[13rem] h-[22rem] bottom-[8rem] lg:h-[81vh]' src={phone} alt='Phone' />
+                    <motion.img initial={{ rotate: -45, scale: 1.6, y: 200, x: 200 }} whileInView={{ rotate: 0, y: 0, x: 0, scale: 1 }} transition={{ duration: 0.8 }} className='relative z-2 min-w-[13rem] right-[5rem] bottom-[5rem] h-[22rem] lg:h-[81vh]' src={leaderboard} alt='Phone' />
                 </div>
             </motion.div>
         </div>
