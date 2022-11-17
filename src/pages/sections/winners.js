f8d705a7cf9e9a41cfe88e27a69249d4f64f24fe
maksat111 <maksatakmyradow111@gmail.com>
2022-11-17 15:56:10 +0500

    added winners

--- src/pages/sections/winners.js
+++ src/pages/sections/winners.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import winners from '../../img/winners.png';
 
 function Winners() {
     return (
         <div>
-            i am winner
+            <h2>Ýeňijiler sanawy</h2>
+            <div className='bg-red'>
+                <div>
+                    <img src={winners}></img>
+                    <div>
+                        <p>Şugün</p>
+                        <p>Hepdelik</p>
+                        <p>Ussat</p>
+                    </div>
+                    <div>
+                        <p>Başlangyç</p>
+                        <p>Höwesjeň</p>
+                        <p>Ussat</p>
+                    </div>
+                    <button>Meni görkez</button>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 }
